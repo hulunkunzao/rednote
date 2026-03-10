048550e48eacb54243ee274ee1abcf24a2a97b3e
AK47are <3318517070@qq.com>
2026-03-10 16:02:27 +0800

    feat(frontend): 添加主布局和导航栏

---
+++ frontend/src/components/Layout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <app-menu />
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+import AppMenu from './AppMenu.vue'
+</script>
+
+<style scoped>
+.common-layout {
+  height: 100vh;
+  width: 100%;
+}
+.common-layout:deep(.el-container) {
+  height: 100%;
+}
+</style>
