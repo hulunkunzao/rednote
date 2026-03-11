032cb0c143c5c1f978372bb611c8d940f89fb2a9
AK47are <3318517070@qq.com>
2026-03-11 21:51:27 +0800

    feat(backend): 实现帖子模型类

---
+++ src/main/java/com/example/rednote/model/dto/PostDTO.java
@@ -0,0 +1,20 @@
+package com.example.rednote.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDTO {
+    private Integer postId;
+    @Schema(description = "用户id", requiredMode = RequiredMode.REQUIRED)
+    private Integer userId;
+    @Schema(description = "帖子标题", requiredMode = RequiredMode.REQUIRED)
+    private String title;
+    @Schema(description = "帖子内容", requiredMode = RequiredMode.REQUIRED)
+    private String content;
+}
---
+++ src/main/java/com/example/rednote/model/po/PostPO.java
@@ -0,0 +1,22 @@
+package com.example.rednote.model.po;
+
+import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+@TableName("post")
+public class PostPO {
+    @TableId(type = IdType.AUTO)
+    private Integer postId;
+    private Integer userId;
+    private String title;
+    private String content;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private LocalDateTime createTime;
+}
