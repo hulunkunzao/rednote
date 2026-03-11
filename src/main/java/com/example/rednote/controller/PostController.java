d27dc9e58952e6d0d9034c510b94985b5b4fbaa2
AK47are <3318517070@qq.com>
2026-03-11 21:53:08 +0800

    feat(backend): 实现获取帖子详情接口

--- src/main/java/com/example/rednote/controller/PostController.java
+++ src/main/java/com/example/rednote/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.example.rednote.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.rednote.common.response.Result;
 import com.example.rednote.model.po.PostPO;
+import com.example.rednote.model.vo.PostVO;
 import com.example.rednote.service.PostService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,9 +18,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping("/posts")
 @Tag(name = "帖子接口", description = "帖子相关接口")
+@AllArgsConstructor
 public class PostController {
     private final PostService postService;
 
@@ -26,5 +29,11 @@ public class PostController {
     public Result<Page<PostPO>> list(@RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "-1") int size) {
         return Result.success(postService.page(new Page<>(page, size)));
+    }
+
+    @GetMapping("/{postId}")
+    @Operation(summary = "获取帖子详情", description = "根据帖子ID返回详细信息")
+    public Result<PostPO> getPost(@PathVariable Integer postId) {
+        return Result.success(postService.getById(postId));
     }
 }
