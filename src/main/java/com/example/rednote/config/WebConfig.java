eeecd753a176666f8f8dcfe085d998288298b81d
AK47are <3318517070@qq.com>
2026-03-13 20:57:39 +0800

    feat(backend): 配置 Jwt Token 拦截器

---
+++ src/main/java/com/example/rednote/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.rednote.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.rednote.config.interceptor.JwtInterceptor;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/users/**", "posts/**")
+                .excludePathPatterns("/users/register", "/users/login");
+    }
+}
