09da4936545adde944d58536cd1d5ccd1d1dbe3d
AK47are <3318517070@qq.com>
2026-03-13 11:37:37 +0800

    feat(backend): 实现用户 Controller、Service、Mapper

---
+++ src/main/java/com/example/rednote/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.rednote.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baomidou.mybatisplus.core.toolkit.BeanUtils;
+import com.example.rednote.common.response.Result;
+import com.example.rednote.model.vo.UserVO;
+import com.example.rednote.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+@Tag(name = "用户接口", description = "用户相关接口")
+public class UserController {
+
+    private final UserService userService;
+}
