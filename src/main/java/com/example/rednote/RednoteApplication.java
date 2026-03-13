93c2cfabd4d943b62495a55cb9797776d8fcb9be
AK47are <3318517070@qq.com>
2026-03-13 17:18:47 +0800

    fix(backend): 消除 MyBatis Bean 重名警告

--- src/main/java/com/example/rednote/RednoteApplication.java
+++ src/main/java/com/example/rednote/RednoteApplication.java
@@ -1,15 +1,13 @@
 package com.example.rednote;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.rednote.mapper")
 public class RednoteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RednoteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RednoteApplication.class, args);
+    }
 
 }
