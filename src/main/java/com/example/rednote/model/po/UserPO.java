bd1a51991d23edfa79d3e90767271b602ea3503d
AK47are <3318517070@qq.com>
2026-03-13 10:22:38 +0800

    feat(backend): 实现用户模型类

---
+++ src/main/java/com/example/rednote/model/po/UserPO.java
@@ -0,0 +1,27 @@
+package com.example.rednote.model.po;
+
+import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@TableName("user")
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserPO {
+
+    @TableId(type = IdType.AUTO)
+    private Integer userId;
+    private String username;
+    private String password;
+    private String avatar;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private LocalDateTime createTime;
+}
