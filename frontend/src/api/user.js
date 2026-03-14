b48dd25ac5b38c7d5648cad1cfa2f9fb29d90d60
AK47are <3318517070@qq.com>
2026-03-14 17:35:14 +0800

    feat(frontend): 添加登录、注册接口

---
+++ frontend/src/api/user.js
@@ -0,0 +1,4 @@
+import request from '@/utils/request'
+
+export const loginApi = (data) => request.post('/users/login', data)
+export const registerApi = (data) => request.post('/users/register', data)
