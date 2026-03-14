ab22e244c2af5a5c664cefe04f43cb2f1fc8b7ce
AK47are <3318517070@qq.com>
2026-03-14 21:39:53 +0800

    feat(backend): 定义 RegisterFailedException 类

---
+++ src/main/java/com/example/rednote/model/exception/RegisterFailedException.java
@@ -0,0 +1,7 @@
+package com.example.rednote.model.exception;
+
+public class RegisterFailedException extends BaseException {
+    public RegisterFailedException(String msg) {
+        super(msg);
+    }
+}
