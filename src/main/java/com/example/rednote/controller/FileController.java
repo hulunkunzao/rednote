47645d54ecae32d1bda3340ac986c50f2a1e715d
hulunkunzao <15688002+huluntunzao@user.noreply.gitee.com>
2026-03-15 18:32:19 +0800

    feat(backend): 实现文件上传接口

---
+++ src/main/java/com/example/rednote/controller/FileController.java
@@ -0,0 +1,30 @@
+package com.example.rednote.controller;
+
+import com.example.rednote.common.response.Result;
+import com.example.rednote.service.FileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+
+@RestController
+@RequestMapping("/files")
+@Tag(name = "文件接口", description = "文件相关接口")
+@AllArgsConstructor
+public class FileController {
+
+    private final FileService fileService;
+
+    @Operation(summary = "帖子图片视频上传")
+    @PostMapping("/update")
+    public Result fileUpdate(@RequestParam("file") MultipartFile file) throws Exception {
+        String fileUrl = fileService.updateFile(file);
+        return Result.success(fileUrl);
+    }
+}
