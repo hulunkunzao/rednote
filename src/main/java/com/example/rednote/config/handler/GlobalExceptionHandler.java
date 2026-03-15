203480b77e24b6aca98efcaa964ef8070777f719
AK47are <3318517070@qq.com>
2026-03-15 10:53:53 +0800

    refactor(backend): 清除全局异常处理器的 log.error
    框架处理异常后会自动记录日志，无需手动使用 log.error，这会导致重复日志。

--- src/main/java/com/example/rednote/config/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/rednote/config/handler/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.example.rednote.config.handler;
 
-import java.sql.SQLIntegrityConstraintViolationException;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,15 +8,12 @@ import com.example.rednote.common.response.Result;
 import com.example.rednote.model.exception.BaseException;
 
 import cn.hutool.http.HttpStatus;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<Result<?>> handle(BaseException e) {
-        log.error("异常信息：{}", e.getMessage());
         return ResponseEntity.status(HttpStatus.HTTP_INTERNAL_ERROR).body(Result.fail(e.getMessage()));
     }
 }
