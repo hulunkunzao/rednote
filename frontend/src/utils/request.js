a71012a14f0bb0a737f6bb39828e48dd72be3dcb
AK47are <3318517070@qq.com>
2026-03-15 12:26:53 +0800

    feat(frontend): 请求拦截器添加 Bearer token

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -7,6 +7,17 @@ const request = axios.create({
   timeout: 10000,
 })
 
+request.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error),
+)
+
 request.interceptors.response.use(
   (response) => response.data,
   (error) => {
