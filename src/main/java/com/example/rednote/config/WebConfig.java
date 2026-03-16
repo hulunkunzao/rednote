f4580d46b5f22a2ecc8c8001dfddc56f3479670a
AK47are <3318517070@qq.com>
2026-03-16 10:16:18 +0800

    fix(backend): /files 相关接口没有 Token 鉴权
    将 /files/** 添加到 Jwt 拦截器拦截路径中。

--- src/main/java/com/example/rednote/config/WebConfig.java
+++ src/main/java/com/example/rednote/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor)
-                .addPathPatterns("/users/**", "posts/**")
+                .addPathPatterns("/users/**", "/posts/**", "/files/**")
                 .excludePathPatterns("/users/register", "/users/login");
     }
 }
