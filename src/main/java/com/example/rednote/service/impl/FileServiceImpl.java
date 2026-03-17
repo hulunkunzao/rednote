79eebfac3463c7acc093309e3192a69b5cb135ea
AK47are <3318517070@qq.com>
2026-03-17 11:32:58 +0800

    refactor(backend): 统一使用 Minio 工具类进行 Minio 相关操作
    数据库仅存储文件 objectName, 将 getPublicUrl 从 FileServiceImpl 提取出来
    ，方便其它类使用。

--- src/main/java/com/example/rednote/service/impl/FileServiceImpl.java
+++ src/main/java/com/example/rednote/service/impl/FileServiceImpl.java
@@ -1,17 +1,13 @@
 package com.example.rednote.service.impl;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.rednote.config.MinioProperties;
+import com.example.rednote.common.utils.MinioUtils;
 import com.example.rednote.service.FileService;
 
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,29 +16,12 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 public class FileServiceImpl implements FileService {
 
-    private final MinioClient minioClient;
-
-    private final MinioProperties minioProperties;
+    private final MinioUtils minioUtils;
 
     @Override
     public String uploadFile(MultipartFile file) throws Exception {
         String objectName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-        minioClient.putObject(
-                PutObjectArgs.builder()
-                        .bucket(minioProperties.getBucketName())
-                        .object(objectName)
-                        .stream(file.getInputStream(), file.getSize(), -1)
-                        .contentType(file.getContentType())
-                        .build());
-        return getPublicUrl(objectName);
-    }
-
-    private String getPublicUrl(String objectName) {
-        String encodedObjectName = URLEncoder.encode(objectName, StandardCharsets.UTF_8)
-                .replace("+", "%20");
-        String publicUrl = minioProperties.getEndpoint() + "/"
-                + minioProperties.getBucketName() + "/"
-                + encodedObjectName;
-        return publicUrl;
+        minioUtils.uploadFile(file, objectName);
+        return minioUtils.getPublicUrl(objectName);
     }
 }
