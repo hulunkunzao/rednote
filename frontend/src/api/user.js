869617c22e67260ece487010998d4760207dd0a4
AK47are <3318517070@qq.com>
2026-03-17 14:22:34 +0800

    feat(frontend): 添加用户基础信息接口

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -3,3 +3,4 @@ import request from '@/utils/request'
 export const loginApi = (data) => request.post('/users/login', data)
 export const registerApi = (data) => request.post('/users/register', data)
 export const getCurrApi = (data) => request.get('/users/current')
+export const getByIdApi = (userId) => request.get(`/users/${userId}`)
