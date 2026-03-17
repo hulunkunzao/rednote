af54d53959cdab9a739fa998a0ab9c6571059c1f
hulunkunzao <15688002+huluntunzao@user.noreply.gitee.com>
2026-03-17 15:12:52 +0800

    feat(backend): 新增帖子详情 po 类

---
+++ src/main/java/com/example/rednote/model/po/PostDetailsPO.java
@@ -0,0 +1,30 @@
+package com.example.rednote.model.po;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+@TableName("post_details")
+public class PostDetailsPO {
+    @TableId(type = IdType.AUTO)
+    private Integer postDetailsId;
+    private Integer postId;
+    private Integer type;
+    private Integer topicIds;
+    private Integer viewCount;
+    private Integer likeCount;
+    private Integer commentCount;
+    private Integer collectCount;
+//    private Integer status;
+//    private String auditRemark;
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "GMT+8")
+//    private LocalDateTime auditTime;
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "GMT+8")
+//    private LocalDateTime createdTime;
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "GMT+8")
+//    private LocalDateTime updatedTime;
+}
