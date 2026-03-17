796f325e16a5f84a45cf411f36c66b42b6856160
hulunkunzao <15688002+huluntunzao@user.noreply.gitee.com>
2026-03-17 15:18:45 +0800

    feat(backend): 新增获取帖子详情接口

--- src/main/java/com/example/rednote/controller/PostDetailsController.java
+++ src/main/java/com/example/rednote/controller/PostDetailsController.java
@@ -2,6 +2,8 @@ package com.example.rednote.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.example.rednote.common.response.Result;
+import com.example.rednote.model.po.PostDetailsPO;
+import com.example.rednote.service.PostDetailsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
@@ -15,5 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "帖子详情接口", description = "帖子详情相关接口")
 @AllArgsConstructor
 public class PostDetailsController {
+    private final PostDetailsService postDetailsService;
 
+    @GetMapping("/{postId}")
+    @Operation(summary = "获取帖子详情", description = "根据帖子ID返回详细信息")
+    public Result<PostDetailsPO> getPost(@PathVariable Integer postId) {
+        LambdaQueryWrapper<PostDetailsPO> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(PostDetailsPO::getPostId, postId);
+        return Result.success(postDetailsService.getOne(wrapper));
+    }
 }
