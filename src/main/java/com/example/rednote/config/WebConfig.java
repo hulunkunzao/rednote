dec846845413ffa71f8ff6659392f97a6e2ed82e
AK47are <3318517070@qq.com>
2026-03-18 11:09:10 +0800

    feat(backend): 将 /follow 添加到 Jwt 拦截器路径

--- src/main/java/com/example/rednote/config/WebConfig.java
+++ src/main/java/com/example/rednote/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor)
-                .addPathPatterns("/users/**", "/posts/**", "/files/**", "/topics/**", "/comments/**")
+                .addPathPatterns("/users/**", "/posts/**", "/files/**", "/topics/**", "/comments/**", "/follow/**")
                 .excludePathPatterns("/users/register", "/users/login");
     }
 }
