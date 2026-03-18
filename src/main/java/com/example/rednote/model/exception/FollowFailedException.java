999e63425430414f2672cb426c8f8916e900589c
AK47are <3318517070@qq.com>
2026-03-18 14:23:57 +0800

    feat(backend): 实现自定义关注失败异常类

---
+++ src/main/java/com/example/rednote/model/exception/FollowFailedException.java
@@ -0,0 +1,7 @@
+package com.example.rednote.model.exception;
+
+public class FollowFailedException extends BaseException {
+    public FollowFailedException(String msg) {
+        super(msg);
+    }
+}
