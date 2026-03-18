7f9c5a20c411ae11341eb1e5b97fd9551cdab053
AK47are <3318517070@qq.com>
2026-03-18 10:31:42 +0800

    feat(frontend): 实现获取当前用户接口

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -2,3 +2,4 @@ import request from '@/utils/request'
 
 export const loginApi = (data) => request.post('/users/login', data)
 export const registerApi = (data) => request.post('/users/register', data)
+export const getCurrApi = (data) => request.get('/users/current')
