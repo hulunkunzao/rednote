18faa4754204292823e2cff8d7e402ae33571fc4
hulunkunzao <2380381330@qq.com>
2026-03-19 14:39:10 +0800

    feat(backend): 新增帖子图片 PO,VO 类

---
+++ src/main/java/com/example/rednote/model/vo/PostImagesVO.java
@@ -0,0 +1,16 @@
+package com.example.rednote.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostImagesVO {
+    private String postId;
+    private List<String> postImageUrls = new ArrayList<>();
+}
