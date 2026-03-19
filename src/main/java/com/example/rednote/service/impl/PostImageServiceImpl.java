8f4134f0b9c83de42bbebfbee60d10174b9ca4c6
hulunkunzao <2380381330@qq.com>
2026-03-19 14:43:23 +0800

    feat(backend): 新增帖子图片 Controller,Mapper,Service 类

---
+++ src/main/java/com/example/rednote/service/impl/PostImageServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.rednote.service.impl;
+import com.example.rednote.service.PostImageService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@AllArgsConstructor
+public class PostImageServiceImpl implements PostImageService {
+
+
+}
