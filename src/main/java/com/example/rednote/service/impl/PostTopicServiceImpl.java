d49f67878efd74e130c543a265810d40b55d7c91
hulunkunzao <2380381330@qq.com>
2026-03-19 14:54:55 +0800

    feat(backend): 新增帖子话题 Mapper,Service 类

---
+++ src/main/java/com/example/rednote/service/impl/PostTopicServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.rednote.service.impl;
+
+import com.example.rednote.service.PostTopicService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostTopicServiceImpl implements PostTopicService {
+
+}
