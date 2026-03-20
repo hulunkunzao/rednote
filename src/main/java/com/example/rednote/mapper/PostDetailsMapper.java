f9ea296ccc59aa6ef5f77f6587d051a13b6a3972
hulunkunzao <2380381330@qq.com>
2026-03-20 15:55:02 +0800

    feat(backend): 帖子详情 Mapper 添加更新点赞总量

--- src/main/java/com/example/rednote/mapper/PostDetailsMapper.java
+++ src/main/java/com/example/rednote/mapper/PostDetailsMapper.java
@@ -3,7 +3,11 @@ package com.example.rednote.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.rednote.model.po.PostDetailsPO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface PostDetailsMapper extends BaseMapper<PostDetailsPO> {
+    @Update("UPDATE post_details SET like_count = like_count + #{delta} WHERE post_id = #{postId}")
+    void updateLikeCount(@Param("postId") Integer postId, @Param("delta") int delta);
 }
